fcb309ff57f40b8657426dd67ddd2ac177ebcc94
LVRUHUA <462528102@qq.com>
2016-10-31 21:53:58 +0800

    node-weixin-servers

---
+++ server/app.js
@@ -0,0 +1,8 @@
+﻿var express = require("express")
+var app = new express()
+var path = require("path")
+var staticpath = path.join(__dirname,"../static");
+app.use(express.static(staticpath))
+app.listen(3000,function(){
+	console.log("open http://127.0.0.1:3000")
+})
