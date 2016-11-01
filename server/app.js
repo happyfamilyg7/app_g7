0f80fdf3eea077c30f791ce1f3952a43c332c86a
LG <462528102@qq.com>
2016-11-01 18:21:17 +0800

    4

--- server/app.js
+++ server/app.js
@@ -3,6 +3,6 @@ var app = new express()
 var path = require("path")
 var staticpath = path.join(__dirname,"../static");
 app.use(express.static(staticpath))
-app.listen(3000,function(){
-	console.log("open http://127.0.0.1:3000")
+app.listen(80,function(){
+	console.log("open http://127.0.0.1:80")
 })
