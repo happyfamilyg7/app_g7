9775ebff7b1774e73603c73ac2662a1c5da0c633
LVRUHUA <462528102@qq.com>
2016-11-05 14:59:18 +0800

    weixin

--- server/app.js
+++ server/app.js
@@ -1,16 +1,32 @@
-
 var express = require("express")
+//post请求处理模块
+var bodyparser = require("body-parser");
+var app = new express();
+//处理post请求，将post请求的数据分装为json
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+
+var express = require("express");
+//post 请求处理模块
+var bodyparser = require("body-parser");
+
+var app = new express();
+//处理 post 请求，将 post 请求的数据封装为 json 
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
 
 
-var app = new express()
-var path = require("path")
+var path = require("path");
 
 require("./token.js")(app);
+require("./jssdk.js")(app);
 
-var staticpath = path.join(__dirname,"../static");
-app.use(express.static(staticpath));
+var saticpath = path.join(__dirname , "../static");
+app.use(express.static(saticpath));
 
-app.listen(80,function(){
-	console.log("open http://127.0.0.1:80")
-})
+app.listen(8000,function(){
+	console.log("open http://127.0.0.1:8000");
+});
 
